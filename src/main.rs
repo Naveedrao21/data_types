e9e636daed58a2fec75444dcdee9c8d8fb6b92d2
Rao Naveed <oyerao18@gmail.com>
2025-03-15 20:55:12 +0500

    using tupple through period

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,10 @@ fn main() {
     let tup = (400,  3.1, 3);
     let (_x, y, _z) = tup;
     println!("the value of y is :{y}" ) ;
+
+    let x = (500, 5.6, 4);
+    let a = x.2;
+    println!("the value of a is :{a}" ) ;
+    let b = x.1;
+    println!("the value of b is :{b}" ) ;
 }
