cfd8ebc5c9f4e33a092b95981b646d37e00a4d49
Rao Naveed <oyerao18@gmail.com>
2025-03-15 21:19:12 +0500

    add array index input and display element value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 fn main() {
     let tup = (400,  3.1, 3);
     let (_x, y, _z) = tup;
@@ -9,6 +10,28 @@ fn main() {
     let b = x.1;
     println!("the value of b is :{b}" ) ;
 
-    let A: () = ();
-    println!("the value of A is :{:?}", A) ;
+    let _A: () = ();
+    println!("the value of A is :{:?}", _A) ;
+
+    let naveed: [u32; 10] = [6; 10];
+    println!("the value of naveed is :{:?}", naveed) ;
+
+    let h = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = h[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
